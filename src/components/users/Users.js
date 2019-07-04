408cd4b67919407b3e447b27ba855645a3eb3210
sj <jainsanmati846@gmail.com>
2019-07-04 21:18:41 +0530

    Initial Commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+export class Users extends Component {
+  state = {
+    users: [
+      {
+        id: '1',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+      },
+      {
+        id: '2',
+        login: 'kbfkg',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+      },
+      {
+        id: '3',
+        login: 'yyyyyy',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
+
+export default Users;
