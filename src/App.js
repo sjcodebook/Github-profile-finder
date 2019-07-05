c66b05495ba0a5ff094c51a1a8a69d92fd0a0d9c
sj <jainsanmati846@gmail.com>
2019-07-05 18:40:19 +0530

    Added search and clear component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
+import { async } from 'q';
 
 class App extends Component {
   state = {
@@ -10,25 +12,48 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }& client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({
+  //     users: res.data,
+  //     loading: false
+  //   });
+  // }
+
+  // Search Github Users
+  searchUsers = async text => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }& client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     });
-  }
+  };
+
+  //Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
