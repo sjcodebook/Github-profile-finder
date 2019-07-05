139c8267891b44e809a24a4bd393cedb2d26c0f2
sj <jainsanmati846@gmail.com>
2019-07-05 13:22:11 +0530

    Added ability to fetch data from github api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: '2',
-        login: 'kbfkg',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: '3',
-        login: 'yyyyyy',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      }
-    ]
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
